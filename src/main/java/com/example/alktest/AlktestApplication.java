ce4d119524d4dff9257830e8b515647d97cfac8f
Nazareno <laraianazareno@gmail.com>
2021-11-16 16:20:56 -0300

    first commit

---
+++ src/main/java/com/example/alktest/AlktestApplication.java
@@ -0,0 +1,13 @@
+package com.example.alktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alktest/AlktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
